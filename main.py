43e5315ad04c437f84c7824764d32da4cda443a0
= <=>
2020-07-02 14:15:10 -0700

    anonymize data

--- main.py
+++ main.py
@@ -482,10 +482,75 @@ df.to_pickle('optionDf.pkl')
 
 df = pd.read_pickle('optionDf.pkl')
 
-
-
-
-out = pd.DataFrame(columns = pd.read_csv('template.csv').columns)
+out = pd.DataFrame(columns = ['Item Type',
+                                'Product ID',
+                                'Product Name',
+                                'Product Type',
+                                'Product Code/SKU',
+                                'Bin Picking Number',
+                                'Brand Name',
+                                'Option Set',
+                                'Option Set Align',
+                                'Product Description',
+                                'Price',
+                                'Cost Price',
+                                'Retail Price',
+                                'Sale Price',
+                                'Fixed Shipping Cost',
+                                'Free Shipping',
+                                'Product Warranty',
+                                'Product Weight',
+                                'Product Width',
+                                'Product Height',
+                                'Product Depth',
+                                'Allow Purchases?',
+                                'Product Visible?',
+                                'Product Availability',
+                                'Track Inventory',
+                                'Current Stock Level',
+                                'Low Stock Level',
+                                'Category',
+                                'Product Image ID - 1',
+                                'Product Image File - 1',
+                                'Product Image Description - 1',
+                                'Product Image Is Thumbnail - 1',
+                                'Product Image Sort - 1',
+                                'Product Image ID - 2',
+                                'Product Image File - 2',
+                                'Product Image Description - 2',
+                                'Product Image Is Thumbnail - 2',
+                                'Product Image Sort - 2',
+                                'Search Keywords',
+                                'Page Title',
+                                'Meta Keywords',
+                                'Meta Description',
+                                'MYOB Asset Acct',
+                                'MYOB Income Acct',
+                                'MYOB Expense Acct',
+                                'Product Condition',
+                                'Show Product Condition?',
+                                'Event Date Required?',
+                                'Event Date Name',
+                                'Event Date Is Limited?',
+                                'Event Date Start Date',
+                                'Event Date End Date',
+                                'Sort Order',
+                                'Product Tax Class',
+                                'Product UPC/EAN',
+                                'Stop Processing Rules',
+                                'Product URL',
+                                'Redirect Old URL?',
+                                'GPS Global Trade Item Number',
+                                'GPS Manufacturer Part Number',
+                                'GPS Gender',
+                                'GPS Age Group',
+                                'GPS Color',
+                                'GPS Size',
+                                'GPS Material',
+                                'GPS Pattern',
+                                'GPS Item Group ID',
+                                'GPS Category',
+                                'GPS Enabled'])
 
 
 dCol = {'Cost Price':'cost',
@@ -621,7 +686,7 @@ comp = pd.read_csv(file)\
 comp.loc[:,ix[0]]=comp.loc[:,ix[0]].str.strip()
 comp.set_index(ix,inplace=True)
 # for production
-# os.rename(file,f'archive/ins/in_{str(end.date())} {end.hour}-{end.minute}-{end.second}.csv')
+os.rename(file,f'archive/ins/in_{str(end.date())} {end.hour}-{end.minute}-{end.second}.csv')
 cols = imgCols\
     +['Product ID','Product Description']
 out = out.reindex(list(set(out.columns.tolist()+cols)),axis=1).set_index(ix)
